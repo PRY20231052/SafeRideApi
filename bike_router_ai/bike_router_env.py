94aad5c0649f8c80a00d444313c29c1de290ef48
pabtejada <65474537+pabtejada@users.noreply.github.com>
2023-09-14 21:01:35 -0500

    api key

--- bike_router_ai/bike_router_env.py
+++ bike_router_ai/bike_router_env.py
@@ -58,7 +58,7 @@ class BikeRouterEnv(Env):
         self.window_aspect_ratio = window_aspect_ratio
         self.clock = None
 
-        GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
+        # GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
         bm.configure(google_maps_api_key='AIzaSyDg0gEkbdTWKu8kRRrc5RdmWnL0LulvaC0')
 
         # Get the city/place Graph and setting origin and destination
