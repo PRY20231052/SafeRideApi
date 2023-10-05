25d778702237e2263e206b343e412bb430fe61a9
Aldair Cuarez <aldaircuarez98@gmail.com>
2023-10-05 16:48:25 -0500

    fix

--- api/views/route_views.py
+++ api/views/route_views.py
@@ -86,8 +86,7 @@ class RouteViewSet(viewsets.ViewSet):
             route.option2 = self._generate_paths_data(graph, dijkstra_paths)
 
             # DEPRECATED
-            # route.paths_geojson = get_routes_as_geojson(graph, paths)
-            route.paths_geojson = {}
+            # route.paths_geojson = get_routes_as_geojson(graph, dijkstra_paths, coords_format='lonlat')
             
             # the serializer variable saves every change done to the route instance
             return Response(serializer.data, status=201) # 201 means CREATED, while 200 only means OK
