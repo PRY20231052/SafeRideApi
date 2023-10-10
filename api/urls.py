92468e02ad5b63c14ac9483d15b9c10a3f4c1e0a
pabtejada <65474537+pabtejada@users.noreply.github.com>
2023-10-10 09:50:39 -0500

    refactoring

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from rest_framework import routers
 from django.urls import path, include
-from api.views import route_views, register_views, favloc_views, trip_views
+from api.views import favorite_location_views, route_views, register_views, trip_views
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'routes', route_views.RouteViewSet, basename='routes')
 router.register(r'trips', trip_views.TripViewSet, basename='trips')
-router.register(r'favorite_locations', favloc_views.FavoriteLocationViewSet, basename='favorite_locations')
+router.register(r'favorite_locations', favorite_location_views.FavoriteLocationViewSet, basename='favorite_locations')
 router.register(r'register', register_views.CustomUserCreateView, basename='register')
 router.register(r'api-token-auth', obtain_auth_token, basename='api_token_auth')
 
--- api/views/favorite_location_views.py
+++ api/views/favorite_location_views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from api.models.favorite_location import FavoriteLocation
-from api.serializers.favloc_serializer import FavoriteLocationSerializer
+from api.serializers.favorite_location_serializer import FavoriteLocationSerializer
 
 class FavoriteLocationViewSet(viewsets.ModelViewSet):
     queryset = FavoriteLocation.objects.all()
