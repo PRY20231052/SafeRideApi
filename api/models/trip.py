71f931f35a3710a7254fa3fa0eecde4aabefc6a0
Aldair Cuarez <aldaircuarez98@gmail.com>
2023-10-16 18:47:26 -0500

    cleaning

--- api/models/trip.py
+++
@@ -1,9 +0,0 @@
-class Trip:
-    def __init__(self, origin, waypoints):
-        # Initial Values
-        self.origin = origin
-        self.waypoints = waypoints # The last waypoint is the destination
-
-        # this will be computed after the prediction by the model
-        self.routes = []
-        self.routes_geojson = {}
--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,6 @@ from api.views import route_views
 
 router = routers.DefaultRouter()
 router.register(r'routes', route_views.RouteViewSet, basename='routes')
-router.register(r'trips', route_views.RouteViewSet, basename='trips')
 
 urlpatterns = [
     path('', include(router.urls)),
